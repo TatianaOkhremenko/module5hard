03d0327091cc39f16457ddfe39018c0ff40c98cb
TatianaOkhremenko <tafinka@mail.ru>
2024-09-19 09:58:58 +0300

    Update module5hard.py

--- module5hard.py
+++ module5hard.py
@@ -1,18 +1,29 @@
 import time
+
+
 class User:
-    def __init__(self, nickname, password, age):
+    def __init__(self, nickname: str, password: int, age: int):
         self.nickname = nickname
-        self.password = hash(password)
+        self.password = password
         self.age = age
 
     def __str__(self):
         return self.nickname
+
+
 class Video:
-    def __init__(self, title, duration, adult_mode=False):
+    def __init__(self, title: str, duration: int, adult_mode: bool = False):
         self.title = title
         self.duration = duration
         self.time_now = 0
         self.adult_mode = adult_mode
+
+    def __eq__(self, other):
+        return self.title == other.title
+
+    def __contains__(self, item):
+        return item in self.title
+
 
 class UrTube:
     def __init__(self):
@@ -20,59 +31,65 @@ class UrTube:
         self.videos = []
         self.current_user = None
 
-    def log_in(self, nickname, password):
-        for user in self.users:
-            if nickname == user.nickname and password == user.password:
-                self.current_user = user
-            elif nickname == user.nickname and password != user.password:
-                print('Не верный пароль')
-            else:
-                print("Такого пользователя не существует")
-
-    def register(self, nickname, password, age):
+    def register(self, nickname: str, password: str, age: int):
         password = hash(password)
         for user in self.users:
-            if nickname == user.nickname:
+            if user.nickname == nickname:
                 print(f"Пользователь {nickname} уже существует")
                 return
+
         new_user = User(nickname, password, age)
-        self.current_user=new_user
         self.users.append(new_user)
+        self.current_user = new_user
+
+
 
     def log_out(self):
         self.current_user = None
+
+    def log_in(self, login: str, password: str):
+        for user in self.users:
+            if login == user.nickname and password == user.password:
+                self.current_user = user
+
     def add(self, *args):
         for movie in args:
-            if movie.title not in [video.title for video in self.videos]:
+            if movie not in self.videos:
                 self.videos.append(movie)
 
-    def get_videos(self, seek_part):
-        rezult = []
+
+
+
+    def get_videos(self, text: str):
+        list_movie = []
         for video in self.videos:
-            if seek_part.upper() in video.title.upper():
-                rezult.append(video.title)
-        return rezult
+            if text.upper() in video.title.upper():
+                list_movie.append(video.title)
+        return list_movie
 
-    def watch_video(self, movie):
-        if self.current_user:
-            for video in self.videos:
-                if self.current_user and self.current_user.age < 18:
-                    print('Вам нет 18 лет, пожалуйста покиньте страницу')
-                    return
-                if movie in video.title:
-                    for i in range(1, 11):
-                        print(i, end=' ')
-                        time.sleep(1)
-                        video.time_now += 1
-                    video.time_now = 0
-                    print('Конец видео')
 
-        else:
+    def watch_video(self, movie: str):
+        if not self.current_user:
             print('Войдите в аккаунт, чтобы смотреть видео')
+            return
+
+        for x in self.videos:
+            if x.title == movie:
+                if x.adult_mode and self.current_user.age < 18:
+                    print('Вам нет 18 лет, пожалуйста, покиньте страницу')
+                    return
+
+                for i in range(x.duration):
+                    print(i, end=' ')
+                    x.time_now += 1
+                x.time_now = 0
+                print('Конец видео')
+
 if __name__ == '__main__':
+
     ur = UrTube()
-v1 = Video('Лучший язык программирования 2024 года', 200)
-v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+    v1 = Video('Лучший язык программирования 2024 года', 200)
+    v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
 # Добавление видео
 ur.add(v1, v2)
